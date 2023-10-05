59d807a956188cc977a91f05c83a796eb7433855
Kuanch <sixigma6@gmail.com>
2023-10-05 15:53:38 +0800

    feat: 375 to 319 cycles

--- hw1/mul32int.s
+++ hw1/mul32int.s
@@ -1,35 +1,35 @@
 .data
-a_val: .word 3         # Sample value for a
-b_val: .word 6         # Sample value for b
+a_val: .word 3
+b_val: .word 6         
 
 .text
 
 main:
-    # Load values of a and b
     la a0, a_val
     la a1, b_val
     
     lw a0, 0(a0)
     lw a1, 0(a1)
 
-    li t0, 0           # i = 0 (loop counter)
-    li t1, 32          # loop bound
-    li t2, 0           # r = 0 (result, lower 32-bits)
+    li t0, 0
+    li t1, 32
+    li t2, 0
 
 loop:
-    # Check loop condition
     bge t0, t1, end_loop
+    addi t0, t0, 1
 
     andi a2, a1, 1
-    beq a2, zero, continue
-
-    add t2, t2, a0
-
-continue:
-    slli a0, a0, 1
     srli a1, a1, 1
+    bnez a2, add_to_result
 
-    addi t0, t0, 1
+    slli a0, a0, 1
+
+    j loop
+
+add_to_result:
+    add t2, t2, a0
+    slli a0, a0, 1
     j loop
 
 
