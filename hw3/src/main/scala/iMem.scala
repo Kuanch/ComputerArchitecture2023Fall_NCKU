06defb6365ef443147b53c1a5eb863070d8eed71
Kuanch <sixigma6@gmail.com>
2023-11-26 21:14:22 +0800

    chores: binary format

--- hw3/src/main/scala/iMem.scala
+++ hw3/src/main/scala/iMem.scala
@@ -1,5 +1,6 @@
 import chisel3._
 import chisel3.util.experimental.loadMemoryFromFile
+import firrtl.annotations.MemoryLoadFileType
 
 class InstructionMemory(size: Int) extends Module {
     val io = IO(new Bundle {
@@ -7,7 +8,7 @@ class InstructionMemory(size: Int) extends Module {
         val instrOut = Output(UInt(32.W))
     })
     val instrMem = Mem(size, UInt(32.W))
-    loadMemoryFromFile(instrMem, "./src/test/instructions.txt")  // hex format
+    loadMemoryFromFile(instrMem, "./src/test/instructions.txt", MemoryLoadFileType.Binary)
 
     io.instrOut := instrMem(io.addr)
 }
--- hw3/src/test/scala/iMemSpec.scala
+++ hw3/src/test/scala/iMemSpec.scala
@@ -9,9 +9,9 @@ class InstructionMemorySpec extends AnyFlatSpec with ChiselScalatestTester with 
     it should "output the correct instruction for a given address" in {
         test(new InstructionMemory(1024)) { c => // Assuming the size of the memory is 1024
             val testInstructions = Seq(
-                "b00000000001000000000001010010011".U, // addi a0, x0, 2
-                "b00000000000100000000001011010011".U,
-                "b00000000101101010000001010110011".U
+                "b00000000000100000000010100010011".U, // addi a0, x0, 1
+                "b00000000001000000000010110010011".U, // addi a1, x0, 2
+                "b00000000101101010000010100110011".U  // add  a0, a0, a1
             )
 
             // Test: Fetch each instruction and compare with expected value
